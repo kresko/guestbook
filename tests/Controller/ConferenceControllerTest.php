c12fcb17f73b8a4ebec3dc73050f6a9ad35e3015
kresko <kresko1995@gmail.com>
2025-08-01 11:16:43 +0200

    Localization added.

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -11,7 +11,7 @@ class ConferenceControllerTest extends PantherTestCase
     public function testIndex()
     {
         $client = static::createPantherClient(['external_base_uri' => rtrim($_SERVER['SYMFONY_PROJECT_DEFAULT_ROUTE_URL'], '/')]);
-        $client->request('GET', '/');
+        $client->request('GET', '/en/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
@@ -20,7 +20,7 @@ class ConferenceControllerTest extends PantherTestCase
     public function testCommentSubmission()
     {
         $client = static::createClient();
-        $client->request('GET', '/conference/amsterdam-2019');
+        $client->request('GET', '/en/conference/amsterdam-2019');
         $client->submitForm('Submit', [
             'comment_type_form[author]' => 'Fabien',
             'comment_type_form[text]' => 'Some feedback from an automated functional test',
@@ -42,7 +42,7 @@ class ConferenceControllerTest extends PantherTestCase
     public function testConferencePage()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/en/');
 
         $this->assertCount(2, $crawler->filter('h4'));
 
@@ -51,6 +51,6 @@ class ConferenceControllerTest extends PantherTestCase
         $this->assertPageTitleContains('Amsterdam');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div', 'There are 1 comments');
+        $this->assertSelectorExists('div:contains("There is one comment")');
     }
 }
